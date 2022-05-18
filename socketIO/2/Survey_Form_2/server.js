dab653d3ab100904f724719e0c70dba21c2f7138
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-05-18 15:18:58 -0700

    update

--- socketIO/2/Survey_Form_2/server.js
+++ socketIO/2/Survey_Form_2/server.js
@@ -4,7 +4,24 @@ app.use(express.static(__dirname + "/static"));
 const server = app.listen(7173);
 const io = require('socket.io')(server);
 
-io.on('connection', function (socket) { 
-  console.log("Someone's listening. ID:" + socket.id)
+// user joined 
+io.on('connection', function (socket) {
+    var lucky = Math.ceil(Math.random()*1000)
 
+    socket.emit('someone_joined', { greeting:`Thank you for joinging ${socket.id} this will be your id number.`, id: socket.id, lucky_number: lucky});
+
+    // message form client that joined 
+    socket.on('client_sent', (from_client) => {
+        console.log(from_client)
+    });
+
+    // telling everyone else this user joined
+    socket.broadcast.emit('new_user', `${socket.id} joined the chat`); 
+    
+    
+    // user left
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('i_left_chat', `${socket.id} left the chat`); 
+        console.log(socket.id, ' Left the server')
+    })
 });
--- socketIO/2/Survey_Form_2/static/js/index.js
+++ socketIO/2/Survey_Form_2/static/js/index.js
@@ -1,4 +1,27 @@
 $(document).ready(()=> {
     const socket = io("http://localhost:7173");
     console.log('Ready to code.');
+    socket.on('someone_joined', (join_from_server) => {
+        console.log(join_from_server.lucky_number);
+
+        console.log(join_from_server.greeting);
+        socket.emit('client_sent', `hello from client ${join_from_server.id}`);
+        
+    });
+
+    // new user that isn't this user joined chat
+    socket.on('new_user', (new_user_joined_info) => {
+        console.log(new_user_joined_info)
+    });
+
+    socket.on('i_left_chat', (message) => {
+        console.log(message);
+    })
+
+    $('form').submit((e) =>{
+        e.preventDefault()
+        console.log('stopped form')
+        console.log($('input[name="name"').val())
+        console.log($('input[name="email"').val())
+    })
 })
