cec023a50659eb13d72d56c8fe84927b90a7f4de
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-05-25 11:46:16 -0700

    changed password and imported password

--- Mongoose/1/hello_world/server.js
+++ Mongoose/1/hello_world/server.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const mongo_connect_info = require('./mongoPass')
 app.use(express.static(__dirname + '/static'));
-
+mongoose.connect(mongo_connect_info, {useNewUrlParser: true});
 app.set('views', __dirname + '/static/views');
 app.set('view engine', 'ejs');
 
+const UserSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+})
+// create an object that contains methods for mongoose to interface with MongoDB
+const User = mongoose.model('User', UserSchema);
+
 app.get('/', function(req, res) {
     res.render("index");
-   })
+})
+
+app.post('/users', (req, res) => {
+    const user = new User();
+    user.name = req.body.name;
+    user.age = req.body.age;
+    user.save()
+    .then(newUserData => console.log('user created: ', newUserData))
+    .catch(err => console.log(err));
+        
+    res.redirect('/');
+    })
+    
 
 
 app.listen(7173, () => {
