1812a1719635c69686e687fd750a19fa647a55e8
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-05-25 14:37:04 -0700

    create done

--- Mongoose/1/hello_world/server.js
+++ Mongoose/1/hello_world/server.js
@@ -21,25 +21,25 @@ const UserSchema = new mongoose.Schema({
 const User = mongoose.model('User', UserSchema);
 
 
-
+// get all the users and display them using ejs (does work if there are no users)
 app.get('/', (req, res) => { 
     User.find()
         .then(data => res.render("index", {users: data}))
         .catch(err => res.json(err));
     });
 
+
+    // send parsed info to the db and create a new user
 app.post('/users', function (req,res) {
-    console.log(req.body)
-    // const user = new User();
-    // user.name = req.body.name;
-    // user.age = req.body.age;
-    // user.save()
-    //   .then(newUserData => console.log('user created: ', newUserData))
-    //   .catch(err => console.log(err));
+    const user = new User();
+    user.name = req.body.name;
+    user.age = req.body.age;
+    user.save()
+      .then(newUserData => console.log('user created: ', newUserData))
+      .catch(err => console.log(err));
         
     res.redirect('/');
     })
-    
 
 app.listen(7173, () => {
     console.log("Listening on port 7173")
