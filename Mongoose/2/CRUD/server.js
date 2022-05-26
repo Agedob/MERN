efa18ee17b2507987cc787d120ff76bf08fba248
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-05-26 13:33:53 -0700

    exported routes to dir got root working

--- Mongoose/2/CRUD/server.js
+++ Mongoose/2/CRUD/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const env = require('dotenv').config();
+require('dotenv').config();
 const mongoose = require('mongoose');
+
 const PORT = process.env.PORT;
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 mongoose.connect(PRIVATE_KEY, {useNewUrlParser: true});
@@ -11,10 +12,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('views', __dirname + '/static/views');
 app.set('view engine', 'ejs'); 
 
+const mongooseRouter = require('./routes/mongooses');
 
-app.get('*', (req,res) => {
-    res.render('index');
-})
+app.use('/mongooses', mongooseRouter)
+
 
  app.listen(PORT, () => { ;
 console.log(`Listening on ${PORT}`);
