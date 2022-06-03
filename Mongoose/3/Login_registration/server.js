e505ddb6fb606ec67e684383cf34afc77954d79f
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-06-02 19:17:02 -0700

    flight testing

--- Mongoose/3/Login_registration/server.js
+++ Mongoose/3/Login_registration/server.js
@@ -4,17 +4,25 @@ const env = require("dotenv").config();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-var bodyParser = require("body-parser");
+
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const bcrypt = require("bcrypt");
+
+var backend = [];
 // mongoose.connect(PRIVATE_KEY, { useNewUrlParser: true });
 app.use(express.static(__dirname + "/static"));
 app.use(express.json());
 app.set("views", __dirname + "/static/views");
 app.set("view engine", "ejs");
 
+const ROUTER = require("./routes/routes");
+
+app.use("/login", ROUTER);
+
 app.get("*", (req, res) => {
-   res.render("index");
+   res.render("index", { DATA: "hello world" });
 });
 
 app.listen(PORT, () => {
