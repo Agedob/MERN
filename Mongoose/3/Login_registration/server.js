347ca2d64c47b79d1b2fd18abc48f72a96c60554
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-09-28 19:26:21 -0700

    ??????

--- Mongoose/3/Login_registration/server.js
+++ Mongoose/3/Login_registration/server.js
@@ -18,7 +18,7 @@ app.use(
    session({
       secret: process.env.SECRET,
       resave: false,
-      saveUninitialized: true,
+      saveUninitialized: false,
       store: MongoStore.create({
          mongoUrl: process.env.PRIVATE_KEY,
       }),
