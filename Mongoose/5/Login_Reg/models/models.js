fda6293b9d6072188bef6dd90cd90d83eb5122cb
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2023-01-04 04:26:53 -0800

    test

---
+++ Mongoose/5/Login_Reg/models/models.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const loginSchema = new mongoose.Schema({
+   first_name: {
+      type: String,
+      required: true,
+   },
+   last_name: {
+      type: String,
+      required: true,
+   },
+   email: {
+      type: String,
+      unique: true,
+      required: true,
+   },
+   password: {
+      type: String,
+      required: true,
+   },
+   birthday: {
+      type: Date,
+      required: true,
+   },
+   date: {
+      type: Date,
+      required: true,
+      default: Date.now,
+   },
+});
+
+module.exports = mongoose.model("Users", loginSchema);
