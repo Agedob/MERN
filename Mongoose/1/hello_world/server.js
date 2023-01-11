bc869a3b3e5b7a39ff3803ab3225f55ea94c7094
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2023-01-11 09:44:24 -0800

    removing node_modz

--- Mongoose/1/hello_world/server.js
+++ Mongoose/1/hello_world/server.js
@@ -1,51 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const mongo_connect_info = require('./mongoPass')
-mongoose.connect(mongo_connect_info, {useNewUrlParser: true});
+const mongo_connect_info = require("./mongoPass");
+mongoose.connect(mongo_connect_info, { useNewUrlParser: true });
 
-app.use(express.static(__dirname + '/static'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/static"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('views', __dirname + '/static/views');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/static/views");
+app.set("view engine", "ejs");
 
-
-const UserSchema = new mongoose.Schema({
-    name:  { type: String, required: true, minlength: 6},
-    age: { type: Number, min: 1, max: 150 },
-},  {timestamps: true });
+const UserSchema = new mongoose.Schema(
+   {
+      name: { type: String, required: true, minlength: 6 },
+      age: { type: Number, min: 1, max: 150 },
+   },
+   { timestamps: true }
+);
 
 // create an object that contains methods for mongoose to interface with MongoDB
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
-const error_message = []
+const error_message = [];
 
 // get all the users and display them using ejs (does work if there are no users)
-app.get('/', (req, res) => { 
+app.get("/", (req, res) => {
+   console.log(User);
+   User.find()
+      .then((data) =>
+         res.render("index", { users: data, errors_from_db: error_message })
+      )
+      .catch((err) => res.json(err));
+});
 
-    User.find()
-        .then(data => res.render("index", {users: data, errors_from_db : error_message}))
-        .catch(err => res.json(err));
+// send parsed info to the db and create a new user
+app.post("/users", function (req, res) {
+   const user = new User();
+   user.name = req.body.name;
+   user.age = req.body.age;
+   user
+      .save()
+      .then((newUserData) => console.log("user created: ", newUserData))
+      .catch((err) => error_message.push(err));
 
+   res.redirect("/");
+});
 
-    });
-
-
-    // send parsed info to the db and create a new user
-app.post('/users', function (req,res) {
-    const user = new User();
-    user.name = req.body.name;
-    user.age = req.body.age;
-    user.save()
-      .then(newUserData => console.log('user created: ', newUserData))
-      .catch(err => error_message.push(err));
-        
-    res.redirect('/');
-    })
-
-    app.listen(7173, () => {
-    console.log("Listening on port 7173")
-})
+app.listen(7173, () => {
+   console.log("Listening on port 7173");
+});
---
+++ Mongoose/5/Login_Reg/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const env = require("dotenv").config();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(PRIVATE_KEY, { useNewUrlParser: true });
+app.use(express.static(__dirname + "/static"));
+app.use(express.json());
+app.set("views", __dirname + "/static/views");
+app.set("view engine", "ejs");
+
+app.get("*", (req, res) => {
+   res.render("index");
+});
+
+app.listen(PORT, () => {
+   console.log(`Listening on ${PORT}`);
+});
