db961db8c259043f101f4d80ea7b74856f896071
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2023-01-12 14:36:05 -0800

    video over

--- React/3/pokemon_app/src/Pagination.js
+++ React/3/pokemon_app/src/Pagination.js
@@ -1,5 +1,12 @@
-const Pagination = () => {
-   return <div>Pagination</div>;
+const Pagination = ({ gotoNextPage, gotoPreviousPage }) => {
+   return (
+      <div>
+         {gotoPreviousPage && (
+            <button onClick={gotoPreviousPage}>Previous</button>
+         )}
+         {gotoNextPage && <button onClick={gotoNextPage}>Next</button>}
+      </div>
+   );
 };
 
 export default Pagination;
