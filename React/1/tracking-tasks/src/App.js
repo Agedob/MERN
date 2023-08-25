285e90eec49c1c3f9d87d4a745a7ec7fd1689713
MЯ.ŠΓΣΣÞÿ <agedob888@gmail.com>
2023-08-25 11:20:18 -0700

    creating global task list in app.js

--- React/1/tracking-tasks/src/App.js
+++ React/1/tracking-tasks/src/App.js
@@ -1,9 +1,33 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [Tasks_list, setTask] = useState([
+    {
+      id: 1,
+      text: "Doctor",
+      day: "Feb 5th at 2pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Gym",
+      day: "Feb 1th at 2pm",
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: "Games",
+      day: "Feb 3th at 2pm",
+      reminder: true,
+    },
+  ]);
+
   return (
     <div className="container">
       <Header title="Task Tracker" />
+      <Tasks Tasks_list={Tasks_list} />
       {/* <h1>Hello world</h1> */}
     </div>
   );
